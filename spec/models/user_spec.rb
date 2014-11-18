b6e8b69b3356b47132b3edc2923fd6d7e8866588
Arvia <arviaglass@Arvias-Cube.local>
2014-11-18 14:21:18 -0800

    second commit with testing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe User do
+
+    it "has an email" do
+      #now use FactoryGirl to build a user
+      user = FactoryGirl.build_stubbed(:a_user)
+      expect(user.email).to_not be_nil  
+    end
+
+    it "has a password" do
+      user = FactoryGirl.build_stubbed(:a_user)
+      expect(user.password).to_not be_nil
+    end
+
+end
